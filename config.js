4c50ccde7add39290d5bedf76ab0a7408fde82d0
nasir.ahmad3073 <nasirnawruzi@gmail.com>
2020-10-06 02:16:21 +0700

    conFusionServer

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '419074109055346',
+        clientSecret: 'a100e9e5512772b2d0e880dd2fb0d996'
+    }
+}
